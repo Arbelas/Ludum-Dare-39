4c81968c8b06a85eb79f3d326d6dd67db373c4dd
Garett Cooper <garettcooper@ymail.com>
2017-07-29 01:10:39 -0400

    End of first night of development. Simple arcady driving mechanics are finished, as well as some rudimentary art assets.

---
+++ Assets/CounerRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounerRotate : MonoBehaviour {
+
+	//This script counters the rotation of the parent object
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles = Vector3.zero;
+	}
+}
---
+++ Assets/FuelGaugeScaler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelGaugeScaler : MonoBehaviour {
+
+	public float PLAYER_POWER;
+	public float MAX_PLAYER_POWER;
+
+	public float zeroPoint;
+
+	private RectTransform rect;
+	private float maxWidth;
+
+	// Use this for initialization
+	void Start() {
+		rect = GetComponent<RectTransform>();
+		maxWidth = rect.sizeDelta.x - zeroPoint;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		//TODO add real player power to this equation
+		rect.sizeDelta = new Vector2(maxWidth * (PLAYER_POWER / MAX_PLAYER_POWER) + zeroPoint, rect.sizeDelta.y);
+	}
+}
